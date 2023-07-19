16c5dba2ffdf3662519d8fad5b448e32bbc0d24f
Han Lee <hanlee4g@gmail.com>
2023-07-18 21:47:48 -0700

    Nav Bar and Search Structure

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,24 @@
-// Component for persistent Happy Birthday Nav Bar
+// Component for persistent happy birthday celebration
 
 import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 function NavBar() {
   return (
-    <div>
-      Nav Bar
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{ backgroundColor: '#62FAA7' }}>
+        <Toolbar>
+          <img src="/assets/party_popper.png" alt="Left Popper" style={{ margin: '0 auto', width: '40px', height: '40px' }} />
+          <Typography variant="h4" fontWeight="bold" color="white" component="div" sx={{ flexGrow: 1 }}>
+            Happy 21st Birthday Haneul!
+          </Typography>
+          <img src="/assets/party_popper.png" alt="Right Popper" style={{ margin: '0 auto', width: '40px', height: '40px' }} />
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,13 +1,32 @@
-// Component for Search Bar
-
 import React from 'react';
+import { Container, TextField } from "@mui/material";
+import { useState } from "react";
+import { Autocomplete } from "@mui/material";
 
 function SearchBar() {
-  return (
-    <div>
-      SearchBar
-    </div>
-  );
+    const [searchTerm, setSearchTerm] = useState("");
+    const keywords = ["video", "photo", "text", "gift", "family", "friends", "other"];
+
+    const handleChange = (event, newValue) => {
+        setSearchTerm(newValue);
+    };
+
+    return (
+        <Container maxWidth="md" sx={{ mt: 5 }}>
+            <Autocomplete
+                id="search"
+                freeSolo
+                options={keywords}
+                renderInput={(params) => (
+                    <TextField {...params}
+                        label="Search"
+                        onChange={handleChange}
+                        sx={{ width: 600 }}
+                    />
+                )}
+            />
+        </Container>
+    );
 }
 
 export default SearchBar;
