d30f754343d711ea785072363eef1775f19ba115
Han Lee <hanlee4g@gmail.com>
2023-07-23 16:36:46 -0700

    auth and login page

---
+++ client/src/pages/login.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../styles/Login.css';
+
+const Login = ({ setPassword }) => {
+  const [isHaneul, setIsHaneul] = useState(false);
+  const [tempPassword, setTempPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (isHaneul && tempPassword === 'password') {
+      navigate('/her', { state: { isAuthenticated: true } });
+    } else {
+      navigate('/');
+    }
+    };
+
+  return (
+    <div className="body-1">
+        <div className="login-box">
+        <img src="../favicon.ico" alt="Your alt text" />
+        <p>Are you Haneul?</p>
+        <div className="buttons">
+            <button onClick={() => setIsHaneul(true)}>Yes</button>
+            <button onClick={() => {
+                setIsHaneul(false);
+                navigate('/not-her');
+            }}>No</button>
+        </div>
+        {isHaneul && (
+            <form onSubmit={handleSubmit}>
+            <label>
+                Password:
+                <input
+                type="password"
+                value={tempPassword}
+                onChange={(e) => setTempPassword(e.target.value)}
+                />
+            </label>
+            <input type="submit" value="Submit" className="input-submit"/>
+            </form>
+        )}
+        </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ client/src/pages/nother.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotHer = () => {
+  return <h1>Welcome, person who is not Haneul!</h1>;
+};
+
+export default NotHer;
